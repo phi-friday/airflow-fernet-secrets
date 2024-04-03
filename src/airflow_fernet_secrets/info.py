7cb408dc87eaf0e3ff69131692cb55be372f968b
phi <phi.friday@gmail.com>
2024-04-03 21:37:51 +0900

    feat: info using yaml

--- src/airflow_fernet_secrets/info.py
+++ src/airflow_fernet_secrets/info.py
@@ -1,97 +1,95 @@
 from __future__ import annotations
 
 import json
+import re
+import shutil
 from importlib.metadata import distribution
 from importlib.util import find_spec
 from pathlib import Path
 from typing import Any
 
-__all__ = ["validate_provider_info"]
+import yaml
+
+__all__ = ["get_provider_info"]
+
+_RE_VARIABLE = re.compile(r"{{ ([a-zA-Z0-9_-]+) }}")
 
 
 def get_provider_info() -> dict[str, Any]:
-    dist = distribution(__package__ or "airflow-fernet-secrets")
+    root = Path(__file__).parent
+    package = __package__ or root.name
+    dist = distribution(package)
+    path = root / "info.yaml"
+    with path.open("r") as file:
+        text = file.read()
+
+    registry = {x: "" for x in _RE_VARIABLE.findall(text)}
     meta = dist.metadata
 
-    return {
-        "package-name": __package__,
-        "name": meta.get("name"),
-        "description": meta.get("summary"),
-        "version": dist.version,
-        "secrets-backends": [
-            "airflow.providers.fernet_secrets.secrets.secret_manager.FernetLocalSecretsBackend"
-        ],
-        "config": {
-            "fernet_secrets": {
-                "description": meta.get("summary"),
-                "options": {
-                    "secret_key": {
-                        "default": None,
-                        "description": "fernet secret key",
-                        "type": "string",
-                        "version_added": "2.8.0",
-                        "example": "96qLkedvLv-7mvHsj5xPOPFbJYUxS3jBp9Q_eLeJOmQ=",
-                    },
-                    "secret_key_cmd": {
-                        "default": None,
-                        "description": "fernet secret key command",
-                        "type": "string",
-                        "version_added": "2.8.0",
-                        "example": "cat /dev/run/fernet_key",
-                    },
-                    "variables_file": {
-                        "default": None,
-                        "description": "variables_file path",
-                        "type": "string",
-                        "version_added": "2.8.0",
-                        "example": "/tmp/variables.sqlite3",  # noqa: S108
-                    },
-                    "variables_file_cmd": {
-                        "default": None,
-                        "description": "variables_file path command",
-                        "type": "string",
-                        "version_added": "2.8.0",
-                        "example": "cat /dev/run/variables",
-                    },
-                    "connections_file": {
-                        "default": None,
-                        "description": "connections_file path",
-                        "type": "string",
-                        "version_added": "2.8.0",
-                        "example": "/tmp/connections.sqlite3",  # noqa: S108
-                    },
-                    "connections_file_cmd": {
-                        "default": None,
-                        "description": "connections_file path command",
-                        "type": "string",
-                        "version_added": "2.8.0",
-                        "example": "cat /dev/run/connections",
-                    },
-                },
-            }
-        },
-    }
+    registry["name"] = meta.get("name") or ""
+    registry["package"] = package
+    registry["version"] = dist.version
+    registry["description"] = meta.get("summary") or ""
+
+    for key, value in registry.items():
+        old = f"{{{{ {key} }}}}"
+        text = text.replace(old, value)
+
+    info = yaml.safe_load(text)
+    validate_provider_info(info)
+    return info
 
 
 def validate_provider_info(info: dict[str, Any]) -> None:
     import jsonschema
 
-    from airflow_fernet_secrets.common.log.common import get_logger
-
-    logger = get_logger()
-
-    spec = find_spec("airflow")
-    if spec is None or spec.origin is None:
-        logger.warning("install airflow first. skip validation.")
+    schema = _get_provider_schema_path()
+    if schema is None:
         return
 
-    origin = Path(spec.origin)
-    schema = origin.with_name("provider_info.schema.json")
-    if not schema.exists() or not schema.is_file():
-        logger.warning("there is no provider info schema file: %s", schema)
+    schema = copy_provider_info_schema(schema)
+    if schema is None:
         return
 
     with schema.open("r") as file:
         json_data: dict[str, Any] = json.load(file)
 
     jsonschema.validate(info, json_data)
+
+
+def copy_provider_info_schema(file: Path | None = None) -> Path | None:
+    local_schema = Path(__file__).with_name("info.schema.json")
+    if local_schema.exists() and not local_schema.is_file():
+        raise FileExistsError(local_schema)
+
+    if file is None:
+        schema = _get_provider_schema_path()
+        if schema is None:
+            return None
+    else:
+        schema = file
+
+    shutil.copyfile(schema, local_schema)
+
+    return local_schema
+
+
+def _get_provider_schema_path() -> Path | None:
+    from airflow_fernet_secrets.common.log.common import get_logger
+
+    logger = get_logger()
+
+    spec = find_spec("airflow")
+    if spec is None or spec.origin is None:
+        logger.warning("install airflow first. skip validation.", stacklevel=2)
+        return None
+
+    origin = Path(spec.origin)
+    schema = origin.with_name("provider_info.schema.json")
+    if not schema.exists() or not schema.is_file():
+        logger.warning(
+            "there is no provider info schema file: %s", schema, stacklevel=2
+        )
+        return None
+
+    return schema
